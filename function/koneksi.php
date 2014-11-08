a61f9292277478a40ca2b3d841ece35cb437f0fa
Anwar Pasaribu <anwargoog@gmail.com>
2014-11-09 00:34:42 +0700

    ubah variable
    Mengubah variable server properties

--- function/koneksi.php
+++ function/koneksi.php
@@ -1,1 +1,10 @@
-<?php	$host = "localhost";	$root = "root";	$password = "root";	$db_name = "pasaribu_store";	    $koneksi = mysql_connect($host,$root,$password) or die (mysql_error() . "<br>Gagal koneksi ke server !!");	$database = mysql_select_db($db_name) or die(mysql_error() . "<br>Gagal memilih database !!");	?>
+<?php
+	$HOST = "localhost";
+	$ROOT = "root";
+	$PASSWORD = "root";
+	$DB_NAME = "pasaribu_store";
+	
+    $koneksi = mysql_connect($HOST,$ROOT,$PASSWORD) or die (mysql_error() . "<br>Gagal koneksi ke server !!");
+	$database = mysql_select_db($DB_NAME) or die(mysql_error() . "<br>Gagal memilih database !!");
+	
+?>
